3ef7ece4596f9b3ed795b48d4f6bbd5de8fb3290
Una Kravets <unakravets@google.com>
2022-01-10 14:53:38 -0500

    single name for q's

--- src/routes/game/question.js
+++ src/routes/game/question.js
@@ -11,19 +11,19 @@ const Question = (props) => {
           </h2>
           <div class="answer-list">
             <div class="answer">
-              <input type="radio" id="a1" name={props.a1} value="a1" />
+              <input type="radio" id="a1" name={props.num} value="a1" />
               <label for="a1">{props.a1}</label>
             </div>
             <div class="answer">
-              <input type="radio" id="a2" name={props.a2} value="a2" />
+              <input type="radio" id="a2" name={props.num} value="a2" />
               <label for="a2">{props.a2}</label>
             </div>
             <div class="answer">
-              <input type="radio" id="a3" name={props.a3} value="a3" />
+              <input type="radio" id="a3" name={props.num} value="a3" />
               <label for="a3">{props.a3}</label>
             </div>
             <div class="answer">
-              <input type="radio" id="a4" name={props.a4} value="a4" />
+              <input type="radio" id="a4" name={props.num} value="a4" />
               <label for="a4">{props.a4}</label>
             </div>
           </div>
