2bb8245e2065d770bb97071a75e4e3a6c3c56690
Una Kravets <unakravets@google.com>
2022-01-10 13:55:31 -0500

    a little style organization

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -7,7 +7,9 @@ const Landing = () => {
 
   return (
     <div class="container">
-      <h1>Big Web Almanac Quiz</h1>
+      <h1>Welcome to the Big Web Almanac Quiz</h1>
+      <p>In this game, you'll have <strong>60 seconds</strong> to answer as many questions as you can about the web today. All questions in this game are statistics pulled from the 2021 <a href="https://almanac.httparchive.org/en/2021/">Web Almanac</a>, and are based on real data from thousands of live websites.</p>
+      <p>So, are you ready?</p>
       <p>
         <Link href="/play" aria-label="Start Quiz">
           Start Quiz
