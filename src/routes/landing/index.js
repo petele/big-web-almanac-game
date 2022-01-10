98713ba425fdf60ed6a2e40fb234591392e386a1
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-01-10 16:31:34 -0500

    MILLIONS

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -8,7 +8,7 @@ const Landing = () => {
   return (
     <div class="container">
       <h1>Welcome to the Big Web Almanac Quiz</h1>
-      <p>In this game, you'll have <strong>60 seconds</strong> to answer as many questions as you can about the web today. All questions in this game are statistics pulled from the 2021 <a href="https://almanac.httparchive.org/en/2021/">Web Almanac</a>, and are based on real data from thousands of live websites.</p>
+      <p>In this game, you'll have <strong>60 seconds</strong> to answer as many questions as you can about the web today. All questions in this game are statistics pulled from the 2021 <a href="https://almanac.httparchive.org/en/2021/">Web Almanac</a>, and are based on real data from millions of live websites.</p>
       <p>So, are you ready?</p>
       <p>
         <Link href="/play" aria-label="Start Quiz">
