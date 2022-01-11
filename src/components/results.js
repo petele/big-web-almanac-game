0c2ed5b00aac0d83c293b17b2e0140a7e4dbc1b1
Rick Viscomi <rviscomi@gmail.com>
2022-01-11 15:24:08 -0500

    share to twitter

--- src/components/results.js
+++ src/components/results.js
@@ -28,18 +28,32 @@ class Results extends Component {
     );
   }
 
+  renderShareButton(correct, total, questions) {
+    const shareText = `The Big Web Almanac Quiz (${correct}/${total})
+
+${questions.map(({answerCorrect}) => answerCorrect ? '🟩' : '⬜️').join('')}
+
+https://big-web-almanac-game.web.app/`;
+
+    return (
+      <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}`}>Share your results</a>
+    );
+  }
+
   render(state) {
     const total = state.questions.questionsPlayed.length
     const correct = state.questions.questionsPlayed.filter(question => {
       return question.answerCorrect;
     }).length;
-    const score = Math.floor(correct / total * 100);
+    const score = total == 0 ? 0 : Math.floor(correct / total * 100);
     this.logScore(score);
 
     return (
       <div class="container">
         <h2>{score}% correct out of <span>{total}!</span></h2>
         <p>Nice job! In 60 seconds, you answered <span>{correct}</span> questions correctly out of <span>{total}</span> total questions attempted.</p>
+
+        {this.renderShareButton(correct, total, state.questions.questionsPlayed)}
 
         {this.renderResponses(state.questions.questionsPlayed)}
       </div>
