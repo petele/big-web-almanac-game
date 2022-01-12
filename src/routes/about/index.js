d011ea0b694a24f133d868262aed52771bc23e9d
Una Kravets <unakravets@google.com>
2022-01-12 15:56:42 -0500

    add to about

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div class="container">
       <h1>The Big Web Almanac Quiz</h1>
-      <p>The Big Web Almanac Quiz is a <a href='https://twitter.com/ChromiumDev'>Chrome DevRel</a> hack week project built by <a href='https://twitter.com/rick_viscomi'>Rick Viscomi</a>, <a href='https://twitter.com/petele'>Pete LePage</a>, and <a href='https://twitter.com/Una'>Una Kravets</a>. All of the data is pulled from the 2021 <a href='https://almanac.httparchive.org/en/2021/'>Web Almanac</a>, which uses data directly sourced from the <a href="https://httparchive.org/">HTTP Archive</a>.</p>
+      <p>The Big Web Almanac Quiz is a <a href='https://twitter.com/ChromiumDev'>Chrome DevRel</a> hackathon project built by <a href='https://twitter.com/rick_viscomi'>Rick Viscomi</a>, <a href='https://twitter.com/petele'>Pete LePage</a>, and <a href='https://twitter.com/Una'>Una Kravets</a>. All of the data is pulled from the 2021 <a href='https://almanac.httparchive.org/en/2021/'>Web Almanac</a>, which uses data directly sourced from the <a href="https://httparchive.org/">HTTP Archive</a>. This is a hackathon project. Don't @ us.</p>
 
       <h2>Gameplay</h2>
       <p>The object of the game is to answer as many questions as you can get right in 60 seconds. You want to maximize for both speed and accuracy. To submit an answer, either click on it with a pointer device or use your keyboard. Once an answer is selected, the game will automatically advance. Good luck!</p>
