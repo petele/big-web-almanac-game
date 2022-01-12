f78a1ee1e190b22adbe9cdae53890dfedf5403b7
Pete LePage <pete.lepage@pobox.com>
2022-01-12 13:44:37 -0500

    image path fixes

--- src/components/header.js
+++ src/components/header.js
@@ -7,14 +7,14 @@ const Header = () => {
     <div class='header'>
       <Link href="/" label="Home" aria-label="Home">
         <picture class="logo">
-          <source srcset="../img/logo.avif" type="image/avif" />
-          <img src="../img/logo.png" />
+          <source srcset="/img/logo.avif" type="image/avif" />
+          <img src="/img/logo.png" />
         </picture>
       </Link>
       <Link href="/about" class='about-btn'>
         <picture>
-          <source srcset="../img/character-file.avif" type="image/avif" />
-          <img src="../img/character-file.png" alt="About the quiz" />
+          <source srcset="/img/character-file.avif" type="image/avif" />
+          <img src="/img/character-file.png" alt="About the quiz" />
         </picture>
       </Link>
     </div>
