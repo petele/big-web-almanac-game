3583acc35b6980aff91111e06571e1faf633c7dc
Pete LePage <pete.lepage@pobox.com>
2022-01-12 15:33:23 -0500

    keyboard

--- src/components/question.js
+++ src/components/question.js
@@ -1,23 +1,48 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-const Question = (props) => {
+// const Question = (props) => {
 
-  return (
-    <div class="question-block" aria-live="polite" tabindex="0">
-      <fieldset class="answers">
-        <h2 class="question">
-          <span class="question-num">{props.num}. </span>
-          {props.q}
-        </h2>
-        <div class="answer-list">
-          <button class="answer" onClick={props.answerClick}>{props.a1}</button>
-          <button class="answer" onClick={props.answerClick}>{props.a2}</button>
-          <button class="answer" onClick={props.answerClick}>{props.a3}</button>
-          <button class="answer" onClick={props.answerClick}>{props.a4}</button>
-        </div>
-      </fieldset>
-    </div>
-  );
-};
+class Question extends Component {
+
+  componentDidMount() {
+    window.addEventListener('keypress', this.keyPressHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keypress', this.keyPressHandler);
+  }
+
+  keyPressHandler(e) {
+    const buttons = document.querySelectorAll('.answer-list button.answer');
+    if (e.key === '1') {
+      buttons[0]?.click();
+    } else if (e.key === '2') {
+      buttons[1]?.click();
+    } else if (e.key === '3') {
+      buttons[2]?.click();
+    } else if (e.key === '4') {
+      buttons[3]?.click();
+    }
+  }
+
+  render(props) {
+    return (
+      <div class="question-block" aria-live="polite" tabindex="0">
+        <fieldset class="answers">
+          <h2 class="question">
+            <span class="question-num">{props.num}. </span>
+            {props.q}
+          </h2>
+          <div class="answer-list">
+            <button class="answer" onClick={props.answerClick}>{props.a1}</button>
+            <button class="answer" onClick={props.answerClick}>{props.a2}</button>
+            <button class="answer" onClick={props.answerClick}>{props.a3}</button>
+            <button class="answer" onClick={props.answerClick}>{props.a4}</button>
+          </div>
+        </fieldset>
+      </div>
+    );
+  }
+}
 
 export default Question;
