501029ad6c9339cdb7074f84a682e75ddf326a4f
Pete LePage <pete.lepage@pobox.com>
2022-01-12 09:55:40 -0500

    remove dead code

--- src/components/timer.js
+++ src/components/timer.js
@@ -11,26 +11,6 @@ const Timer = (props) => {
   const [percent, setPercent] = useState(1);
 
   useEffect(() => {
-    // function step() {
-    //   if (remaining > 0) {
-    //     const remainMS = endAt - Date.now();
-    //     const remainSec = Math.round(remainMS / 1000);
-    //     const pct = remainMS / (numSec * 1000);
-    //     const pctRemaining = `${pct * 100}%`;
-    //     document.body.style.setProperty("--timer", pctRemaining);
-    //     setPercent(pct);
-    //     setRemaining(remainSec);
-    //     requestAnimationFrame(step);
-    //   } else {
-    //     document.body.style.setProperty("--timer", "0%");
-    //   }
-    // }
-
-    // let val = requestAnimationFrame(step);
-
-    // return () => {
-    //   cancelAnimationFrame(val);
-    // }
 
     let myInterval = setInterval(() => {
       if (remaining > 0) {
